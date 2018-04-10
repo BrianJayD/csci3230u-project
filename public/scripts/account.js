38e6cbaa0ad82ae1a53a24dbaa66be114fe13ea7
BrianJayD <brian.jay1795@gmail.com>
2018-04-10 17:21:38 -0400

    [Added] account.js Javascript for account page.

---
+++ public/scripts/account.js
@@ -0,0 +1,14 @@
+
+// JavaScript Document
+
+$(window).ready(function() {
+	'use strict';
+
+	$('#btn_reset').click(function() {
+    $('#uname_input').val("Username");
+    $('#pass_input').val("password");
+    $('#email_input').val("email@email.com");
+  })
+
+
+});
